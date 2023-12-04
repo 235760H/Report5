aa5a24359bc97df1e26e6679e507d962eaa76d6f
SOUMA Arakaki <e235760@ie.u-ryukyu.ac.jp>
2023-12-04 22:16:40 +0900

    2nd

--- exception.java
+++ exception.java
@@ -1,6 +1,11 @@
 public class exception {
     public static void main(String[] args){
+        try{
         String str = null;
         System.out.println(str.length());
+        } catch (NullPointerException str){
+        System.out.println("NullPointerExceptionが発生しました。");
+        System.out.println(str.getMessage());
+        }        
     }
 }
